28298534045519bb20aa317cbf2f157b2b2fb8b4
Reed Bryan <reed.f.bryan@gmail.com>
2025-07-18 16:39:15 -0700

    remove big comment

--- Assets/Scripts/Cameras/PanningManager.cs
+++ Assets/Scripts/Cameras/PanningManager.cs
@@ -54,11 +54,3 @@ public class PanningManager : MonoBehaviour
         cameraParent.rotation = Quaternion.Euler(initialRotation.x, newY, initialRotation.z);
     }
 }
-
-/*write a readme for my unity project. the project takes 360 videos (dropped into the streaming assets folder) and outputs a cubemapped equirectangular projection of that footage into 3 displays. it is ment to be used in a space with 3 projectors, projecting onto evenly sized walls so that it looks like you are surrounded by the footage. there are features to edit the post processing of each display (brightness, saturation, contrast, etc) to account for any inconsistancy in the different projectors. also the tilt of each displays camera is editable (to fix any alignment issues at runntime). all these settings can be saved presets and reloaded accross multiple sessions. you can also rotate the cameras to get a different viewpoint or activate a rotate setting that will slowly pan the camera on its own. 
-
-when writing make references to code (i've attached the repo below) and if something is better demonstrated with an image then leave a placeholder and I take add that later.
-
-Github: https://github.com/reedbryan/cubemap-projection-mediaplayer 
-
-*/
