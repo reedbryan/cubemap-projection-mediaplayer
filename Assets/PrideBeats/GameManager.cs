fa065c1dc0bbfda025b7c8b3f4884b212b5de1dd
Reed Bryan <reed.f.bryan@gmail.com>
2025-08-27 12:32:14 -0700

    Change startGame osc message to send the PC's IP. This way the quest can read that IP and assign it to the hostIP

--- Assets/PrideBeats/GameManager.cs
+++ Assets/PrideBeats/GameManager.cs
@@ -1,12 +1,9 @@
-
-
 using UnityEngine;
 using extOSC;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
-
 
 public class GameManager : MonoBehaviour
 {
@@ -16,19 +13,24 @@ public class GameManager : MonoBehaviour
     public ConfigVideoSource configVideoSource;
     private OSCTransmitter transmitter;
 
-
     public List<ScreenEffects> ScreenEffectss = new List<ScreenEffects>();
     private Dictionary<string, ScreenEffects> IPs = new Dictionary<string, ScreenEffects>();
 
+    // Store local IP for reuse
+    private string localIPAddress;
+
     void Awake()
     {
-        // Transmitter to broadcast address
+        // Get and store local IP once
+        localIPAddress = GetLocalIPAddress();
+        Debug.Log("[GameManager] Local IP: " + localIPAddress);
+
+        // Setup transmitter to broadcast
         transmitter = gameObject.AddComponent<OSCTransmitter>();
         string broadcastIP = NetworkUtils.GetBroadcastAddress();
-        Debug.Log("broadcastIP"  + broadcastIP);
+        Debug.Log("Broadcast IP: " + broadcastIP);
         transmitter.RemoteHost = broadcastIP;
-        transmitter.RemotePort = 8100; // Or whatever port your Quest devices are listening on
-
+        transmitter.RemotePort = 8100; // Port that Quests are listening on
 
         Debug.Log("[GameManager] Ready to broadcast to all Quests.");
     }
@@ -36,12 +38,12 @@ public class GameManager : MonoBehaviour
     private bool GameOn = false;
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Space))
-       {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             StartGame();
             GameOn = true;
             Debug.Log("Starting game");
-       }
+        }
     }
 
     public List<float> startSequence = new List<float> { 0.5f, 1.0f, 0.75f }; // Example pattern
@@ -55,9 +57,13 @@ public class GameManager : MonoBehaviour
             startMsg.AddValue(OSCValue.Float(value));
         }
 
-        transmitter.Send(startMsg);
-        Debug.Log("[GameManager] Broadcasted /StartGame with " + startSequence.Count + " intervals");
+        // ✅ Add the local IP as a string argument
+        startMsg.AddValue(OSCValue.String(localIPAddress));
 
+        transmitter.Send(startMsg);
+        Debug.Log("[GameManager] Broadcasted /StartGame with " + startSequence.Count + " intervals + IP " + localIPAddress);
+
+        // Play video
         string fileName = "Test2_360_200Mbps.mp4";
         string fullPath = Path.Combine(Application.streamingAssetsPath, fileName);
         GameObject.Find("VideoSkyboxPlayer").GetComponent<ConfigVideoSource>().PlayVideo(fullPath);
@@ -65,13 +71,11 @@ public class GameManager : MonoBehaviour
         Debug.Log("[GameManager] Started pridebeats video");
     }
 
-
     public void DrumHit(string IP, string content)
     {
-        // 1. Assign a ScreenEffects to the IP if not already assigned - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Assign ScreenEffects if not already mapped
         if (!IPs.TryGetValue(IP, out ScreenEffects ScreenEffects))
         {
-            // Assign the next unassigned ScreenEffects
             foreach (var effect in ScreenEffectss)
             {
                 if (!IPs.ContainsValue(effect))
@@ -91,17 +95,15 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        // Log message content
+        // Handle sync messages
         if (content.Contains("in sync"))
         {
             Debug.Log(IP + " in sync");
-            // Activate the effect
             ScreenEffects.ActivateEffects(true);
         }
         else if (content.Contains("out of sync"))
         {
             Debug.Log(IP + " out of sync");
-            // Activate the effect
             ScreenEffects.ActivateEffects(false);
         }
         else
@@ -110,4 +112,23 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // 🔹 Utility to get local IPv4 address
+    private string GetLocalIPAddress()
+    {
+        string ipAddress = "0.0.0.0";
+        try
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                ipAddress = endPoint.Address.ToString();
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogWarning($"[GameManager] Could not determine local IP: {ex.Message}");
+        }
+        return ipAddress;
+    }
 }
